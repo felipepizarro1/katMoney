4862aa6a98921dd159b6baaa7565708e05427e06
Felipe <fpizarro.ux@gmail.com>
2022-04-11 11:46:26 +0200

    modalReactStrap

--- src/components/AddBudgetModal.js
+++ src/components/AddBudgetModal.js
@@ -1,61 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Modal, ModalHeader, Input, ModalBody, FormGroup, Label, Form, Button } from 'reactstrap'
+import { useRef } from 'react'
 
 
 
-class AddBudgetModal extends Component {
+export default function AddBudgetModal({ show, handleClose }) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          modal: false
-        };
-    
-        this.toggle = this.toggle.bind(this);
-      }
-    
-      toggle() {
-        this.setState({
-          modal: !this.state.modal
-        });
-      }
-      handleSubmit() {
-          console.log("handleSubmit")
-      }
+    function handleSubmit(e) {
 
+    }
+    const nameRef =  useRef()
+    const maxRef = useRef()
 
-
-
- render () {
   return (
-      <>
-        <Button outline color='transparent' size="sm" onClick={this.toggle}>
-          Click Here
-        </Button>
-        <Modal isOpen={this.state.modal} >
-            <Form onSubmit={this.handleSubmit}>
-                <ModalHeader toggle={this.toggle}>
-                    New Budget
-                </ModalHeader>
-                <ModalBody>
-                    <FormGroup className="mb-3" controlId="name">
-                        <Label>Name</Label>
-                        <Input type="text" required></Input>
-                    </FormGroup>
-                    <FormGroup className="mb-3" controlId="max">
-                        <Label>Maximum Spending</Label>
-                        <Input type="number" required min={0} step={0.01} ></Input>
-                    </FormGroup>
-                    <div className="d-flex justify-content-end">
-                        <Button color="primary" type="submit">Add</Button>
-                    </div>
-                </ModalBody>
-            </Form>
-        </Modal>
-    </>
+    <Modal isOpen={show} onHide={handleClose} >
+        <Form onSubmit={handleSubmit}>
+            <ModalHeader closeButton>
+                New Budget
+            </ModalHeader>
+            <ModalBody>
+                <FormGroup className="mb-3" controlId="name">
+                    <Label>Name</Label>
+                    <Input type="text" required></Input>
+                </FormGroup>
+                <FormGroup className="mb-3" controlId="max">
+                    <Label>Maximum Spending</Label>
+                    <Input type="number" required min={0} step={0.01} ></Input>
+                </FormGroup>
+                <div className="d-flex justify-content-end">
+                    <Button color="primary" type="submit">Add</Button>
+                </div>
+            </ModalBody>
+        </Form>
+    </Modal>
   )
 }
-}
-
-export default AddBudgetModal;
  
